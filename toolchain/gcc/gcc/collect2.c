2b61a9662cf459adc21d48e927278324f9d28ed5
Alvaro <alvieboy@alvie.com>
2010-03-29 20:09:00 +0100

    Add file mode to open() with O_CREAT

--- toolchain/gcc/gcc/collect2.c
+++ toolchain/gcc/gcc/collect2.c
@@ -1532,7 +1532,7 @@ collect_execute (const char *prog, char **argv, const char *redir)
   if (redir)
     {
       /* Open response file.  */
-      redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT);
+      redir_handle = open (redir, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 
       /* Duplicate the stdout and stderr file handles
 	 so they can be restored later.  */
